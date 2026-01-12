4d7be1f2de319b29efeb88706f0687cd08ccfee1
matheusldeperes <matheusldeperes@gmail.com>
2026-01-12 10:59:53 -0300

    dois links

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ count = st_autorefresh(interval=30000, key="framereload")
 st.cache_data(ttl=60)
 links = [
     "https://app.powerbi.com/view?r=eyJrIjoiMmUxMjMzMmQtM2FhMC00NmMyLThkYzQtM2I2OTNlY2M4MjA1IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9",
+    "https://app.powerbi.com/view?r=eyJrIjoiMmUxMjMzMmQtM2FhMC00NmMyLThkYzQtM2I2OTNlY2M4MjA1IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9",
    # "https://app.powerbi.com/view?r=eyJrIjoiMTI5MDM0M2QtMGU1ZS00ODE3LWI0NGMtZTcwYWIyMDA0NGQ4IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9"
 ]
 
