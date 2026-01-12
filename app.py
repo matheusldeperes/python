71eea36721e705e2bbe53e4338695f69ea4d3dab
matheusldeperes <matheusldeperes@gmail.com>
2026-01-12 18:13:54 -0300

    60s 700 height

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from streamlit_autorefresh import st_autorefresh
 st.set_page_config(layout="wide")
 
 # Atualiza a página a cada 30 segundos
-count = st_autorefresh(interval=30000, key="framereload")
+count = st_autorefresh(interval=60000, key="framereload")
 
 # Lista de links (Use o link de "Publicar na Web")
 links = [
@@ -19,4 +19,4 @@ links = [
 indice = count % len(links)
 
 # Exibindo o dashboard
-st.components.v1.iframe(links[indice], height=500, scrolling=False)
+st.components.v1.iframe(links[indice], height=700, scrolling=False)
