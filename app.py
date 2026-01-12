6c614f5363c383de24cbbc74e46b50ad0433b678
matheusldeperes <matheusldeperes@gmail.com>
2026-01-12 11:27:00 -0300

    refresh 10min + links diferentes para a mesma página

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from streamlit_autorefresh import st_autorefresh
 # Configuração da página para ocupar a tela toda
 st.set_page_config(layout="wide")
 
-# Atualiza a página a cada 30 segundos
-count = st_autorefresh(interval=30000, key="framereload")
+# Atualiza a página a cada 10 minutos
+count = st_autorefresh(interval=600000, key="framereload")
 
 # Lista de links (Use o link de "Publicar na Web")
 links = [
     "https://app.powerbi.com/view?r=eyJrIjoiMmUxMjMzMmQtM2FhMC00NmMyLThkYzQtM2I2OTNlY2M4MjA1IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9",
-    "https://app.powerbi.com/view?r=eyJrIjoiMTI5MDM0M2QtMGU1ZS00ODE3LWI0NGMtZTcwYWIyMDA0NGQ4IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9"
+    "https://app.powerbi.com/view?r=eyJrIjoiMmUxMjMzMmQtM2FhMC00NmMyLThkYzQtM2I2OTNlY2M4MjA1IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9&pageName=c9e5107072363ab3728b",
 ]
+    #"https://app.powerbi.com/view?r=eyJrIjoiMTI5MDM0M2QtMGU1ZS00ODE3LWI0NGMtZTcwYWIyMDA0NGQ4IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9"
+
 
 # Lógica para alternar o link baseado no contador de refresh
 indice = count % len(links)
