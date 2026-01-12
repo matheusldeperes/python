e22e14e65c92176b91c80e4981ef809bf5056026
matheusldeperes <matheusldeperes@gmail.com>
2026-01-12 11:12:25 -0300

    add o outro link denovo

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ count = st_autorefresh(interval=30000, key="framereload")
 # Lista de links (Use o link de "Publicar na Web")
 links = [
     "https://app.powerbi.com/view?r=eyJrIjoiMmUxMjMzMmQtM2FhMC00NmMyLThkYzQtM2I2OTNlY2M4MjA1IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9",
-    "https://app.powerbi.com/view?r=eyJrIjoiMmUxMjMzMmQtM2FhMC00NmMyLThkYzQtM2I2OTNlY2M4MjA1IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9",
-   # "https://app.powerbi.com/view?r=eyJrIjoiMTI5MDM0M2QtMGU1ZS00ODE3LWI0NGMtZTcwYWIyMDA0NGQ4IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9"
+    "https://app.powerbi.com/view?r=eyJrIjoiMTI5MDM0M2QtMGU1ZS00ODE3LWI0NGMtZTcwYWIyMDA0NGQ4IiwidCI6IjgwNGM1M2Y3LTIwNWEtNDI4NS1hNjhmLWVjOTU4NzllOTYzYiJ9"
 ]
 
 # Lógica para alternar o link baseado no contador de refresh
